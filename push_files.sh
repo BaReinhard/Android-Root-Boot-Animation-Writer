7d590281a73143a34a7819a68513e550ab025cb9
Brett Reinhard <brettreinhard@gmail.com>
2017-11-18 09:38:57 -0800

    Fixed wrong file name

--- push_files.sh
+++ push_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Pushing Files......"
 adb push boot_animation_mover.sh /storage/sdcard0/Scripts/
-adb push boot_animation_mover.sh /storage/sdcard0/Scripts/
+adb push shut_animation_mover.sh /storage/sdcard0/Scripts/
 echo "Files Pushed!"
 echo "Exiting"
