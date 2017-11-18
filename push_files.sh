218b0cc1395766392976dee03e1752b0ca0069a1
Brett Reinhard <brettreinhard@gmail.com>
2017-11-18 09:36:43 -0800

    Create push_files.sh
    Created file to move files to /storage/sdcard0/Scripts

---
+++ push_files.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Pushing Files......"
+adb push boot_animation_mover.sh /storage/sdcard0/Scripts/
+adb push boot_animation_mover.sh /storage/sdcard0/Scripts/
+echo "Files Pushed!"
+echo "Exiting"
